575eb067d916d10a8f409b8c676bd9bedbe9258a
Mahamadou75-19 <gadiarramahamadou@gmail.com>
2026-04-12 16:16:47 +0200

    task00

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
-"""Module async_generator"""
+"""Async Generator"""
 
-import asyncio
-import random
+from asyncio import sleep
+from random import uniform
+from typing import Generator
 
 
-async def async_generator():
-    """Génère 10 nombres aléatoires entre 0 et 10"""
+async def async_generator() -> Generator[float, None, None]:
+    """Coroutine that generate 10 random number enter 0 to 10."""
     for _ in range(10):
-        await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+        await sleep(1)
+        yield uniform(0, 10)
