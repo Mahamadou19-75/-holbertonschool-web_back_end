b008e97fc7eb1d3909148c0e97701b05eeb75f47
Mahamadou75-19 <gadiarramahamadou@gmail.com>
2026-04-30 01:36:31 +0200

    task8

---
+++ ES6_classes/8-hbtn_class.js
@@ -0,0 +1,23 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    if (typeof size !== 'number') {
+      throw new TypeError('size must be a number');
+    }
+    if (typeof location !== 'string') {
+      throw new TypeError('location must be a string');
+    }
+
+    this._size = size;
+    this._location = location;
+  }
+
+  // Cast to number → return size
+  valueOf() {
+    return this._size;
+  }
+
+  // Cast to string → return location
+  toString() {
+    return this._location;
+  }
+}
