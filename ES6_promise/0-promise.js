0a076c082b3aa990e693f9a34a7bf35dfc3f320c
Mahamadou75-19 <gadiarramahamadou@gmail.com>
2026-05-07 10:22:16 +0200

    task0

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,10 @@
+/**
+ * Retourne une nouvelle Promesse.
+ * @returns {Promise} Une instance de Promise.
+ */
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // Pour cet exercice, on n'a pas besoin de logique interne spécifique,
+    // on retourne juste l'objet Promise.
+  });
+}
---
+++ ES6_promise/README.md
@@ -0,0 +1,4 @@
+This project contains JavaScript scripts for manipulating student data.
+The main function `getListStudents` returns an array of formatted objects.
+Use `npm run dev` followed by the filename to execute the tests.
+Developed as part of learning JavaScript data structures.
