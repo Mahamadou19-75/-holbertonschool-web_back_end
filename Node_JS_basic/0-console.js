930445abbb2cbf6e995192d90d64ebb27c4f750c
Mahamadou75-19 <gadiarramahamadou@gmail.com>
2026-05-11 14:48:09 +0200

    task0

---
+++ Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * Simple function to print a message to the standard output.
+ * @param {string} message - The string to be printed.
+ */
+function displayMessage(message) {
+  console.log(message);
+}
+
+module.exports = displayMessage; 
