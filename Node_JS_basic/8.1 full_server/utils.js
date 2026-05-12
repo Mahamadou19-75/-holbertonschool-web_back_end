9da3626aae8e5e1722c1aaf55b30bec05d26b45b
Mahamadou75-19 <gadiarramahamadou@gmail.com>
2026-05-12 14:17:33 +0200

    task5

---
+++ Node_JS_basic/8.1 full_server/utils.js
@@ -0,0 +1,25 @@
+import fs from 'fs';
+
+export const readDatabase = (path) => new Promise((resolve, reject) => {
+  fs.readFile(path, 'utf8', (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+      return;
+    }
+
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+    const studentLines = lines.slice(1);
+    const fields = {};
+
+    for (const line of studentLines) {
+      const studentData = line.split(',');
+      if (studentData.length >= 4) {
+        const firstName = studentData[0];
+        const field = studentData[3];
+        if (!fields[field]) fields[field] = [];
+        fields[field].push(firstName);
+      }
+    }
+    resolve(fields);
+  });
+});
