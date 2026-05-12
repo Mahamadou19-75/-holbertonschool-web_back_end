798c4956612f811b51aea98e9e690651316f84b5
Mahamadou75-19 <gadiarramahamadou@gmail.com>
2026-05-12 14:36:46 +0200

    task6

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -1,18 +1,14 @@
 const express = require('express');
 
-// Création de l'instance de l'application Express
-const app = express();
 const port = 1245;
+const app = express();
 
-// Définition de la route pour l'index /
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-// Le serveur écoute sur le port 1245
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is listening on port ${port}`);
 });
 
-// Exportation de l'application
 module.exports = app;
