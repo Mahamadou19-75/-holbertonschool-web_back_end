4378555ce3740252d49a414c1ab54b1974553ad9
Mahamadou75-19 <gadiarramahamadou@gmail.com>
2026-05-12 14:45:26 +0200

    task5

--- Node_JS_basic/5-main.js
+++ Node_JS_basic/5-main.js
@@ -1,8 +1,30 @@
-const app = require('./5-http.js');
+const http = require('http');
+const countStudents = require('./3-read_file_async');
 
-// On vérifie simplement si l'objet app est bien exporté
-if (app) {
-  console.log('Server app is successfully exported!');
-} else {
-  console.log('Export failed.');
-}
+const port = 1245;
+
+const app = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+
+  if (req.url === '/') {
+    res.statusCode = 200;
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    countStudents(process.argv[2])
+      .then((response) => {
+        res.statusCode = 200;
+        res.end(`This is the list of our students\n${response}`);
+      })
+      .catch((error) => {
+        res.statusCode = 500;
+        res.end(`This is the list of our students\n${error.message}`);
+      });
+  } else {
+    res.statusCode = 404;
+    res.end('Not found.');
+  }
+});
+
+app.listen(port);
+
+module.exports = app;
